17613243b72c87660c3770c4a22190c84a2fb0b2
JustDalo <shishlodan@mail.ru>
2021-04-09 23:37:55 +0300

    add automatic background image change

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ const time = document.getElementById("time"),
 	  name = document.getElementById("name"),
 	  focus = document.getElementById("focus");
 
+const body = document.querySelector('body');
+const btn = document.querySelector('.backgroundBtn');
+
 //Show Time 
 function showTime() {
 //	let today = new Date(2021, 04, 07, 17, 33, 30),
@@ -79,6 +82,88 @@ function setBgGreet() {
 	}	
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max + 1);
+}
+
+const images = [];
+
+function getBackGroundImages() {
+	let random;
+	for (let i = 0; i < 24; i++) {
+		random = getRandomInt(20);
+		images.push(random + ".jpg");
+	}
+	console.log(images);
+}
+
+const nightBase = 'img/Night/';
+const morningBase = 'img/Morning/';
+const afternoonBase = 'img/Afternoon/';
+const eveningBase = 'img/Evening/';
+let currTime = new Date(),
+	currHour = currTime.getHours();
+let j = currHour - 1;
+function changeBackground() {
+	let timeLeft = new Date(),
+		hour = timeLeft.getHours(),
+		min = timeLeft.getMinutes(),
+		sec = timeLeft.getSeconds();
+	const index = j % 24;
+	let currBase;
+	
+	if (index < 6) {
+		currBase = nightBase;
+	}
+	else if (index < 12) {
+		currBase = morningBase;
+	}
+	else if (index < 18) {
+		currBase = afternoonBase;	
+	}
+	else {
+		currBase = eveningBase;	
+	}	
+	const imageSrc = currBase + images[index];
+	console.log(imageSrc);
+	viewBackGroundImg(imageSrc);
+	j++;
+	setTimeout(changeBackground, (60 - sec) * 1000);
+}
+
+let i = currHour;
+function getImage() {
+	const index1 = i % images.length;
+	let currBase;
+	
+	if (index1 < 6) {
+		currBase = nightBase;
+	}
+	else if (index1 < 12) {
+		currBase = morningBase;
+	}
+	else if (index1 < 18) {
+		currBase = afternoonBase;	
+	}
+	else {
+		currBase = eveningBase;	
+	}	
+  	const imageSrc1 = currBase + images[index1];
+  	viewBackGroundImg(imageSrc1);
+  	i++;
+  	btn.disabled = true;
+  	setTimeout(function() { btn.disabled = false }, 1000);
+} 
+
+function viewBackGroundImg(src) {
+	const img = new Image();
+	img.src = src;
+	console.log(src);
+	img.onload = () => {      
+    	body.style.backgroundImage = `url(${src})`;
+  	}; 
+}
+
 //Get Name 
 function getName() {
 	if (localStorage.getItem('name') === null || localStorage.getItem('name') === '') {
@@ -151,8 +236,12 @@ name.addEventListener('blur', setName);
 focus.addEventListener('keypress', setFocus);
 focus.addEventListener('blur', setFocus);
 
+btn.addEventListener('click', getImage);
+
 //Run
 setBgGreet();
+getBackGroundImages();
+changeBackground();
 showTime();
 showDate();
 getName();
